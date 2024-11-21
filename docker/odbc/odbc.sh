ea0237c1237334bc733e858fe02bdcc516a690f0
Douglas Portugal <douglfabian@gmail.com>
2024-11-21 10:39:44 -0300

    suporte a db2 removido script

--- docker/odbc/odbc.sh
+++ docker/odbc/odbc.sh
@@ -3,7 +3,5 @@ curl https://packages.microsoft.com/config/rhel/9/prod.repo | tee /etc/yum.repos
 ACCEPT_EULA=Y microdnf install -y tar gzip openssl libaio unixODBC-devel msodbcsql18 mariadb-connector-odbc postgresql-odbc && \
 rpm -Uvh https://download.oracle.com/otn_software/linux/instantclient/2350000/oracle-instantclient-basic-23.5.0.24.07-1.el9.x86_64.rpm && \
 rpm -Uvh https://download.oracle.com/otn_software/linux/instantclient/2350000/oracle-instantclient-sqlplus-23.5.0.24.07-1.el9.x86_64.rpm && \
-rpm -Uvh https://download.oracle.com/otn_software/linux/instantclient/2350000/oracle-instantclient-odbc-23.5.0.24.07-1.el9.x86_64.rpm && \
-tar -zxvf ./ibm_data_server_driver_for_odbc_cli.tar.gz -C /etc/zabbix/ && \
-rm -f ./ibm_data_server_driver_for_odbc_cli.tar.gz && \
+rpm -Uvh https://download.oracle.com/otn_software/linux/instantclient/2350000/oracle-instantclient-odbc-23.5.0.24.07-1.el9.x86_64.rpms
 microdnf clean all
