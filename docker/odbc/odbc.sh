eb7a34272a56cb53e49a963047a1695f9b6caf4d
Douglas Portugal <douglfabian@gmail.com>
2024-11-21 11:53:42 -0300

    corrigido erro escrit pacote

--- docker/odbc/odbc.sh
+++ docker/odbc/odbc.sh
@@ -3,5 +3,5 @@ curl https://packages.microsoft.com/config/rhel/9/prod.repo | tee /etc/yum.repos
 ACCEPT_EULA=Y microdnf install -y tar gzip openssl libaio unixODBC-devel msodbcsql18 mariadb-connector-odbc postgresql-odbc && \
 rpm -Uvh https://download.oracle.com/otn_software/linux/instantclient/2350000/oracle-instantclient-basic-23.5.0.24.07-1.el9.x86_64.rpm && \
 rpm -Uvh https://download.oracle.com/otn_software/linux/instantclient/2350000/oracle-instantclient-sqlplus-23.5.0.24.07-1.el9.x86_64.rpm && \
-rpm -Uvh https://download.oracle.com/otn_software/linux/instantclient/2350000/oracle-instantclient-odbc-23.5.0.24.07-1.el9.x86_64.rpms
+rpm -Uvh https://download.oracle.com/otn_software/linux/instantclient/2350000/oracle-instantclient-odbc-23.5.0.24.07-1.el9.x86_64.rpm
 microdnf clean all
