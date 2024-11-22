f9dafd07f9e30aa6c737f58198328f51fafca038
Douglas Portugal <douglfabian@gmail.com>
2024-11-22 11:37:22 -0300

    freetds add

--- docker/odbc/odbc.sh
+++ docker/odbc/odbc.sh
@@ -1,9 +1,13 @@
 cd ./ && \
-curl https://packages.microsoft.com/config/rhel/9/prod.repo | tee /etc/yum.repos.d/mssql-release.repo && \
-ACCEPT_EULA=Y microdnf install -y tar gzip openssl libaio unixODBC-devel msodbcsql17 msodbcsql18 mariadb-connector-odbc postgresql-odbc && \
+microdnf install -y wget openssl tar gzip libaio unixODBC unixODBC-devel mariadb-connector-odbc postgresql-odbc && \
+rpm -Uvh https://packages.microsoft.com/rhel/9.0/prod/Packages/m/msodbcsql17-17.10.6.1-1.x86_64.rpm && \
+rpm -Uvh https://packages.microsoft.com/rhel/9.0/prod/Packages/m/msodbcsql18-18.4.1.1-1.x86_64.rpm && \
 rpm -Uvh https://download.oracle.com/otn_software/linux/instantclient/2350000/oracle-instantclient-basic-23.5.0.24.07-1.el9.x86_64.rpm && \
 rpm -Uvh https://download.oracle.com/otn_software/linux/instantclient/2350000/oracle-instantclient-sqlplus-23.5.0.24.07-1.el9.x86_64.rpm && \
 rpm -Uvh https://download.oracle.com/otn_software/linux/instantclient/2350000/oracle-instantclient-odbc-23.5.0.24.07-1.el9.x86_64.rpm && \
+rpm -Uvh https://kojipkgs.fedoraproject.org//packages/freetds/1.4.23/1.el9/x86_64/freetds-1.4.23-1.el9.x86_64.rpm && \
+rpm -Uvh https://kojipkgs.fedoraproject.org//packages/freetds/1.4.23/1.el9/x86_64/freetds-devel-1.4.23-1.el9.x86_64.rpm && \
+rpm -Uvh https://kojipkgs.fedoraproject.org//packages/freetds/1.4.23/1.el9/x86_64/freetds-libs-1.4.23-1.el9.x86_64.rpm && \
 sed -i '1s/^/openssl_conf = default_conf\n/' /etc/ssl/openssl.cnf && \
     echo "" >> /etc/ssl/openssl.cnf && \
     echo "[ default_conf ]" >> /etc/ssl/openssl.cnf && \
@@ -15,5 +19,5 @@ sed -i '1s/^/openssl_conf = default_conf\n/' /etc/ssl/openssl.cnf && \
     echo "[system_default_sect]" >> /etc/ssl/openssl.cnf && \
     echo "MinProtocol = TLSv1" >> /etc/ssl/openssl.cnf && \
     echo "CipherString = DEFAULT:@SECLEVEL=0" >> /etc/ssl/openssl.cnf && \
-sed -i "s/TLS.MinProtocol = TLSv1.2/TLS.MinProtocol = TLSv1.0/" /etc/crypto-policies/back-ends/opensslcnf.config
+sed -i "s/TLS.MinProtocol = TLSv1.2/TLS.MinProtocol = TLSv1.0/" /etc/crypto-policies/back-ends/opensslcnf.config && \
 microdnf clean all
