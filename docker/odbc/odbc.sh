fd1a2723c7e0d3debc96d780810a12bfbc568f49
Douglas Portugal <douglfabian@gmail.com>
2024-11-22 09:31:48 -0300

    fix syntax

--- docker/odbc/odbc.sh
+++ docker/odbc/odbc.sh
@@ -3,20 +3,17 @@ curl https://packages.microsoft.com/config/rhel/9/prod.repo | tee /etc/yum.repos
 ACCEPT_EULA=Y microdnf install -y tar gzip openssl libaio unixODBC-devel msodbcsql17 msodbcsql18 mariadb-connector-odbc postgresql-odbc && \
 rpm -Uvh https://download.oracle.com/otn_software/linux/instantclient/2350000/oracle-instantclient-basic-23.5.0.24.07-1.el9.x86_64.rpm && \
 rpm -Uvh https://download.oracle.com/otn_software/linux/instantclient/2350000/oracle-instantclient-sqlplus-23.5.0.24.07-1.el9.x86_64.rpm && \
-rpm -Uvh https://download.oracle.com/otn_software/linux/instantclient/2350000/oracle-instantclient-odbc-23.5.0.24.07-1.el9.x86_64.rpm && 
-sed -i '1s/^/openssl_conf = default_conf\n/' /etc/ssl/openssl.cnf && 
-\
+rpm -Uvh https://download.oracle.com/otn_software/linux/instantclient/2350000/oracle-instantclient-odbc-23.5.0.24.07-1.el9.x86_64.rpm && \
+sed -i '1s/^/openssl_conf = default_conf\n/' /etc/ssl/openssl.cnf && \
     echo "" >> /etc/ssl/openssl.cnf && \
     echo "[ default_conf ]" >> /etc/ssl/openssl.cnf && \
     echo "ssl_conf = ssl_sect" >> /etc/ssl/openssl.cnf && \
     echo "" >> /etc/ssl/openssl.cnf && \
     echo "[ssl_sect]" >> /etc/ssl/openssl.cnf && \
-    echo "system_default = system_default_sect" >> /etc/ssl/openssl.cnf 
-&& \
+    echo "system_default = system_default_sect" >> /etc/ssl/openssl.cnf && \
     echo "" >> /etc/ssl/openssl.cnf && \
     echo "[system_default_sect]" >> /etc/ssl/openssl.cnf && \
     echo "MinProtocol = TLSv1" >> /etc/ssl/openssl.cnf && \
-    echo "CipherString = DEFAULT:@SECLEVEL=0" >> /etc/ssl/openssl.cnf
-&& \
+    echo "CipherString = DEFAULT:@SECLEVEL=0" >> /etc/ssl/openssl.cnf && \
 sed -i "s/TLS.MinProtocol = TLSv1.2/TLS.MinProtocol = TLSv1.0/" /etc/crypto-policies/back-ends/opensslcnf.config
 microdnf clean all
