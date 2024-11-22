a03ed6de0e37c07f5415546f3b388934208d6209
Douglas Portugal <douglfabian@gmail.com>
2024-11-22 12:19:20 -0300

    free tds new

--- docker/odbc/odbc.sh
+++ docker/odbc/odbc.sh
@@ -1,13 +1,11 @@
 cd ./ && \
-microdnf install -y wget openssl tar gzip libaio unixODBC unixODBC-devel mariadb-connector-odbc postgresql-odbc && \
+microdnf install -y openssl tar gzip libaio unixODBC unixODBC-devel mariadb-connector-odbc postgresql-odbc && \
 rpm -Uvh https://packages.microsoft.com/rhel/9.0/prod/Packages/m/msodbcsql17-17.10.6.1-1.x86_64.rpm && \
 rpm -Uvh https://packages.microsoft.com/rhel/9.0/prod/Packages/m/msodbcsql18-18.4.1.1-1.x86_64.rpm && \
 rpm -Uvh https://download.oracle.com/otn_software/linux/instantclient/2350000/oracle-instantclient-basic-23.5.0.24.07-1.el9.x86_64.rpm && \
 rpm -Uvh https://download.oracle.com/otn_software/linux/instantclient/2350000/oracle-instantclient-sqlplus-23.5.0.24.07-1.el9.x86_64.rpm && \
 rpm -Uvh https://download.oracle.com/otn_software/linux/instantclient/2350000/oracle-instantclient-odbc-23.5.0.24.07-1.el9.x86_64.rpm && \
-rpm -Uvh https://kojipkgs.fedoraproject.org//packages/freetds/1.4.23/1.el9/x86_64/freetds-1.4.23-1.el9.x86_64.rpm && \
-rpm -Uvh https://kojipkgs.fedoraproject.org//packages/freetds/1.4.23/1.el9/x86_64/freetds-devel-1.4.23-1.el9.x86_64.rpm && \
-rpm -Uvh https://kojipkgs.fedoraproject.org//packages/freetds/1.4.23/1.el9/x86_64/freetds-libs-1.4.23-1.el9.x86_64.rpm && \
+rpm -Uvh https://kojipkgs.fedoraproject.org//packages/freetds/1.4.23/1.el9/x86_64/freetds-libs-1.4.23-1.el9.x86_64.rpm https://kojipkgs.fedoraproject.org//packages/freetds/1.4.23/1.el9/x86_64/freetds-1.4.23-1.el9.x86_64.rpm https://kojipkgs.fedoraproject.org//packages/freetds/1.4.23/1.el9/x86_64/freetds-devel-1.4.23-1.el9.x86_64.rpm && \
 sed -i '1s/^/openssl_conf = default_conf\n/' /etc/ssl/openssl.cnf && \
     echo "" >> /etc/ssl/openssl.cnf && \
     echo "[ default_conf ]" >> /etc/ssl/openssl.cnf && \
