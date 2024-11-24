0de49bbe520857eb6c6dde2ad5cfdddc7150f1b5
Douglas Portugal <douglfabian@gmail.com>
2024-11-24 10:13:12 -0300

    ajust script to build new image

--- docker/odbc/build.sh
+++ docker/odbc/build.sh
@@ -4,5 +4,8 @@ DCKPATH=/home/douglas/Documents/git/zabbix/docker/odbc
 IMAGENAME=douglasportugal/zbx-prx-sqlite3
 IMAGETAG=7.0.6
 
+# Reminder to change Dockerfile first line to same image tag version before execute this script
+
 docker images | grep -i ${IMAGENAME} | awk '{print $3}' | xargs docker rmi -f
+
 docker build -t ${IMAGENAME}:${IMAGETAG} -f ${DCKPATH}/Dockerfile ${DCKPATH}
