d00bd3fe3b6a400937828a8de89f55b6773bbdf8
Douglas Portugal <73395119+douglasportugal@users.noreply.github.com>
2025-03-09 08:45:45 -0300

    Update build.sh
    update image tag

--- docker/odbc/build.sh
+++ docker/odbc/build.sh
@@ -2,7 +2,7 @@
 
 DCKPATH=/home/douglas/Documents/git/zabbix/docker/odbc
 IMAGENAME=douglasportugal/zbx-prx-sqlite3
-IMAGETAG=7.0.6
+IMAGETAG=7.0.10
 
 # Reminder to change Dockerfile first line to same image tag version before execute this script
 
